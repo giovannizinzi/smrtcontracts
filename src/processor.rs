3ab1fc35191c3f239e9435b3b42a72dad82dd3e5
giovannizinzi <jzooms@gmail.com>
2021-09-27 01:05:50 +0000

    logic start

---
+++ src/processor.rs
@@ -0,0 +1,44 @@
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint::ProgramResult,
+    program_error::ProgramError,
+    msg,
+    pubkey::Pubkey,
+};
+
+use crate::instruction::EscrowInstruction;
+
+pub struct Processor;
+impl Processor {
+    pub fn process(program_id: &Pubkey, accounts: &[AccountInfo], instruction_data: &[u8]) -> ProgramResult {
+        let instruction = EscrowInstruction::unpack(instruction_data)?;
+
+        match instruction {
+            EscrowInstruction::InitEscrow { amount } => {
+                msg!("Instruction: InitEscrow");
+                Self::process_init_escrow(accounts, amount, program_id)
+            }
+        }
+    }
+
+    fn process_init_escrow(
+        accounts: &[AccountInfo],
+        amount: u64,
+        program_id: &Pubkey,
+    ) -> ProgramResult {
+        let account_info_iter = &mut accounts.iter();
+        let initializer = next_account_info(account_info_iter)?;
+        //https://docs.rs/solana-program/1.5.1/solana_program/account_info/fn.next_account_info.html <- helpful to look at what next account info actually is
+        if !initializer.is_signer {
+            return Err(ProgramError::MissingRequiredSignature);
+        }
+
+        let temp_token_account = next_account_info(account_info_iter)?;
+        let token_to_receive_account = next_account_info(account_info_iter)?;
+        if *token_to_receive_account.owner != spl_token::id() {
+            return Err(ProgramError::IncorrectProgramId);
+        }
+        Ok(())
+    }
+    
+}
